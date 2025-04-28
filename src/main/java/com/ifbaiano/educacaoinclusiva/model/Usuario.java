3aaf55a84cdefe80757c7494a4bf9d8957719b72
Tainara Amaral <tainaraazevedo791@gmail.com>
2025-04-28 20:46:10 -0300

    classe usuário

--- src/main/java/com/ifbaiano/educacaoinclusiva/model/Usuario.java
+++ src/main/java/com/ifbaiano/educacaoinclusiva/model/Usuario.java
@@ -1,5 +1,66 @@
 package com.ifbaiano.educacaoinclusiva.model;
 
 public class Usuario {
+	private int id;
+	private String nome;
+	private String email;
+	private String senha;
+	private String bio;
+	private String avaliacao;
 
+	public Usuario(int id, String nome, String email, String senha, String bio) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.senha = senha;
+		this.bio = bio;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getBio() {
+		return bio;
+	}
+
+	public void setBio(String bio) {
+		this.bio = bio;
+	}
+	
+	public void posta(String conteudo) {
+		setAvaliacao(conteudo);
+	}
+
+	public String getAvaliacao() {
+		return avaliacao;
+	}
+
+	public void setAvaliacao(String avaliacao) {
+		this.avaliacao = avaliacao;
+	}
 }
