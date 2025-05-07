184a15f785fbcdfd7a0cc8cc6ec344be05775186
FarleyOliveira <farley219@hotmail.com>
2025-05-07 00:10:59 -0300

    Alterações em Modulo
    Sem paciencia, Ctrl C e Ctrl V

--- src/main/java/com/ifbaiano/educacaoinclusiva/model/Modulo.java
+++ src/main/java/com/ifbaiano/educacaoinclusiva/model/Modulo.java
@@ -9,7 +9,14 @@ public class Modulo {
     private int idCurso;
     private List<Videoaula> videoaulas;
 
-    public Modulo(int id, String titulo, String descricao, List<String> videoaulas){
+    public Modulo(int id, String titulo, String descricao, int idCurso) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.idCurso = idCurso;
+    }
+
+    public Modulo(int id, String titulo, String descricao, List<Videoaula> videoaulas) {
         this.id = id;
         this.titulo = titulo;
         this.descricao = descricao;
@@ -19,32 +26,40 @@ public class Modulo {
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getTitulo() {
         return titulo;
     }
+
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
+
     public String getDescricao() {
         return descricao;
     }
+
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
-    public List<Videoaula> getVideoaulas() {
-        return videoaulas;
-    }
-    public void setVideoaulas(List<Videoaula> videoaulas) {
-        this.videoaulas = videoaulas;
-    }
+
     public int getIdCurso() {
         return idCurso;
     }
 
     public void setIdCurso(int idCurso) {
         this.idCurso = idCurso;
+    }
+
+    public List<Videoaula> getVideoaulas() {
+        return videoaulas;
+    }
+
+    public void setVideoaulas(List<Videoaula> videoaulas) {
+        this.videoaulas = videoaulas;
     }
 }
