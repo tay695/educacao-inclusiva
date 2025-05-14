52bbac51d73c5ad8c9e760d9648a2e47db7ece45
Tainara Amaral <tainaraazevedo791@gmail.com>
2025-05-13 23:14:17 -0300

    atualizações em tutor

---
+++ src/main/java/com/ifbaiano/educacaoinclusiva/DAO/TutorDAO.java
@@ -0,0 +1,69 @@
+package com.ifbaiano.educacaoinclusiva.DAO;
+
+import com.ifbaiano.educacaoinclusiva.config.DBConfig;
+import com.ifbaiano.educacaoinclusiva.model.Tutor;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TutorDAO {
+
+	private Connection conexao;
+
+	public TutorDAO() {
+		this.conexao = DBConfig.criarConexao();
+	}
+
+	public void adicionarTutor(Tutor tutor) {
+		String sql = "INSERT INTO tutor (nome, email, areaEspecializacao) VALUES (?, ?, ?)";
+
+		try {
+			PreparedStatement stmt = conexao.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+			stmt.setString(1, tutor.getRetornaNome());
+			stmt.setString(2, tutor.getEmail());
+			stmt.setString(3, tutor.getAreaEspecializacao());
+
+			stmt.executeUpdate();
+
+			ResultSet rs = stmt.getGeneratedKeys();
+			if (rs.next()) {
+				int idGerado = rs.getInt(1);
+				tutor.setId(idGerado);
+			}
+
+			stmt.close();
+		} catch (SQLException e) {
+			System.out.println("Houve um erro ao inserir" + e);
+		}
+	}
+
+	// Atualizar tutor
+	public void atualizarTutor(Tutor tutor) {
+		String sql = "UPDATE tutor SET nome = ?, email = ? WHERE id = ?";
+
+		try {
+			PreparedStatement stmt = conexao.prepareStatement(sql);
+			stmt.setString(1, tutor.getRetornaNome());
+			stmt.setString(2, tutor.getEmail());
+			stmt.setInt(3, tutor.getId());
+			stmt.executeUpdate();
+			stmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// deletar
+	public void excluirTutor(int id) {
+		String sql = "DELETE FROM tutor WHERE id = ?";
+
+		try {
+			PreparedStatement stmt = conexao.prepareStatement(sql);
+			stmt.setInt(1, id);
+			stmt.executeUpdate();
+			stmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
--- src/main/java/com/ifbaiano/educacaoinclusiva/model/Tutor.java
+++ src/main/java/com/ifbaiano/educacaoinclusiva/model/Tutor.java
@@ -1,11 +1,16 @@
 package com.ifbaiano.educacaoinclusiva.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tutor extends Usuario {
 	private String areaEspecializacao;
+	private List<Videoaula> videoaulas;
 
 	public Tutor(String areaEspecializacao, int id, String nome, String email, String senha, String bio) {
 		super(id, nome, email, senha, bio);
 		this.areaEspecializacao = areaEspecializacao;
+		   this.videoaulas = new ArrayList<>();
 	}
 
 	public void setAreaEspecializacao(String areaEspecializacao) {
@@ -15,4 +20,11 @@ public class Tutor extends Usuario {
 	public String getAreaEspecializacao() {
 		return areaEspecializacao;
 	}
+
+    public void postarVideoaula(String titulo, String url, int idModulo) {
+        int novoId = videoaulas.size() + 1;
+        Videoaula video = new Videoaula(novoId, titulo, url, idModulo);
+        videoaulas.add(video);
+        System.out.println("Videoaula postada com sucesso: " + titulo);
+    }
 }
