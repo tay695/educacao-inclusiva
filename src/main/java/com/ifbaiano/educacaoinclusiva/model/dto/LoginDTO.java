80cc0f35a5f3504e2a867c2cfa4f7a3214b8ff04
Tainara Amaral <tainaraazevedo791@gmail.com>
2025-05-19 08:49:15 -0300

    refatorção em login, adicionando o DTO

---
+++ src/main/java/com/ifbaiano/educacaoinclusiva/model/dto/LoginDTO.java
@@ -0,0 +1,20 @@
+package com.ifbaiano.educacaoinclusiva.model.dto;
+
+public class LoginDTO {
+	
+	    private String email;
+	    private String senha;
+
+	    public LoginDTO(String email, String senha) {
+	        this.email = email;
+	        this.senha = senha;
+	    }
+
+	    public String getEmail() { return email; }
+	    public void setEmail(String email) { this.email = email; }
+
+	    public String getSenha() { return senha; }
+	    public void setSenha(String senha) { this.senha = senha; }
+	}
+
+
